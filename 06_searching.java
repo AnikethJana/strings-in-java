79fda6b994146024d8d2018c2466c6afa8c54f5e
AnikethJana <96902730+AnikethJana@users.noreply.github.com>
2023-02-05 23:20:39 +0530

    searching e

---
+++ 06_searching.java
@@ -0,0 +1,14 @@
+public class assign {
+    public static void main(String[] args) {
+        
+        String s1 = "Umbrella";
+        int i;
+        char e;
+        if(s1.indexOf('e')!=-1){
+            System.out.println("e is present .");
+        }
+        else{
+            System.out.println(" e is not present .");
+        }
+    }
+}
