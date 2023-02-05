7bb5dd6a5e18bf8287de2ee4aff1e7f4fecf0245
AnikethJana <96902730+AnikethJana@users.noreply.github.com>
2023-02-05 22:32:59 +0530

    length

---
+++ 03_length-of_string.java
@@ -0,0 +1,12 @@
+// for fetching length of a string we use length();
+
+import java.util.Scanner;
+
+public class assign {
+    public static void main(String[] args) {
+        
+      
+        String s1 = "I am Aniketh";
+        System.out.println(s1.length());
+    }
+}
