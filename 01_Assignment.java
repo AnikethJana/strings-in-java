962689b5050d18312dffe67234245a6ae9ec3a76
AnikethJana <96902730+AnikethJana@users.noreply.github.com>
2023-02-05 21:04:50 +0530

    string input

---
+++ 01_Assignment.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class assign {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Give String : ");
+        String scan = s.nextLine();
+        System.out.println("input is : "+scan);
+    }
+}
